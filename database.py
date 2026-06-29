727f2224b3ed70faddb75b4857e3b5279e01be11
Abdel-Moneim Khaled <bodikhaled929@gmail.com>
2026-06-29 16:37:28 +0300

    Add functions to load and save students data

---
+++ database.py
@@ -0,0 +1,17 @@
+import json
+import os
+
+FILE_NAME = "data.json"
+
+
+def load_students():
+    if not os.path.exists(FILE_NAME):
+        return []
+
+    with open(FILE_NAME, "r") as file:
+        return json.load(file)
+
+
+def save_students(students):
+    with open(FILE_NAME, "w") as file:
+        json.dump(students, file, indent=4)
